8f665bcbffaad4a1d73c1e0dacca90225ed09e77
Patrick Wolff <pat.wolff81@gmail.com>
2022-11-27 20:14:01 -0600

    Adding Exercise 6.3

---
+++ week-6/gpa-calculator-app1/src/app/base-layout/base-layout.component.ts
@@ -0,0 +1,27 @@
+/**
+ * Title: base-layout.component.ts
+ * Author: Professor Krasso
+ * Date: 27 November 2022
+ * Modified by: Patrick wolff
+ * Description: base layout component
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-base-layout',
+  templateUrl: './base-layout.component.html',
+  styleUrls: ['./base-layout.component.css']
+})
+export class BaseLayoutComponent implements OnInit {
+
+  assignment: string;
+
+  constructor() {
+    this.assignment = 'Exercise 6.3 - Layouts';
+   }
+
+  ngOnInit(): void {
+  }
+
+}
